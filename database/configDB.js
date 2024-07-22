aaf151b5c35c64ad4bee926d225579a8a5d940f4
DWWM-3 <hans.lederle1609@mail.com>
2024-07-22 09:12:07 +0200

    push

---
+++ database/configDB.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoDb: {
+    uri: "mongodb+srv://hanslederle1609:Akira2303@cluster0.3e9zu9q.mongodb.net/websocket?retryWrites=true&w=majority&appName=Cluster0",
+  },
+};
---
+++ server.js
@@ -0,0 +1,96 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { nanoid } from "nanoid";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const games = {};
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("createGame", () => {
+    const gameId = nanoid();
+    games[gameId] = {
+      players: [socket.id],
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      currentPlayer: socket.id,
+    };
+    socket.emit("gameCreated", { gameId });
+  });
+
+  socket.on("joinGame", ({ gameId }) => {
+    if (games[gameId] && games[gameId].players.length === 1) {
+      games[gameId].players.push(socket.id);
+      socket.emit("gameJoined", { gameId, playerId: socket.id });
+      io.to(games[gameId].players[0]).emit("gameUpdate", {
+        squares: games[gameId].squares,
+        xIsNext: games[gameId].xIsNext,
+        currentPlayer: games[gameId].currentPlayer,
+      });
+      io.to(socket.id).emit("gameUpdate", {
+        squares: games[gameId].squares,
+        xIsNext: games[gameId].xIsNext,
+        currentPlayer: games[gameId].currentPlayer,
+      });
+    } else {
+      socket.emit("error", { message: "Game not found or already full" });
+    }
+  });
+
+  socket.on("makeMove", ({ gameId, index, value }) => {
+    if (
+      games[gameId] &&
+      games[gameId].squares[index] === null &&
+      games[gameId].currentPlayer === socket.id
+    ) {
+      games[gameId].squares[index] = value;
+      games[gameId].xIsNext = !games[gameId].xIsNext;
+      games[gameId].currentPlayer = games[gameId].players.find(
+        (player) => player !== socket.id
+      );
+      const winner = calculateWinner(games[gameId].squares);
+      io.to(games[gameId].players[0]).emit("gameUpdate", {
+        squares: games[gameId].squares,
+        xIsNext: games[gameId].xIsNext,
+        currentPlayer: games[gameId].currentPlayer,
+      });
+      io.to(games[gameId].players[1]).emit("gameUpdate", {
+        squares: games[gameId].squares,
+        xIsNext: games[gameId].xIsNext,
+        currentPlayer: games[gameId].currentPlayer,
+      });
+      if (winner) {
+        io.to(games[gameId].players[0]).emit("statusUpdate", winner);
+        io.to(games[gameId].players[1]).emit("statusUpdate", winner);
+      }
+    }
+  });
+});
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
